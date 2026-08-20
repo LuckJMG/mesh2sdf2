f22e77bce26183f222ce4f384718b2664d6a1f94
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-08-20 13:40:56 -0400

    feat!: drop Python 3.6-3.9, modernize build deps
    - Drop support for Python 3.6-3.9 (requires-python >=3.10)
    
    - Bump setuptools floor to 68, pybind11 to 2.13
    
    - Remove wheel from build-system requires (auto-provided by setuptools)
    
    - Move license to SPDX string form, drop deprecated license classifier
    
    - Remove .python-version pin (superseded by requires-python)
    
    - Refresh uv.lock
    
    BREAKING CHANGE: requires-python now >=3.10; v1.x will not install on Python 3.6-3.9.

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup
-# Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-__version__ = '1.1.0'
-
-
-with open("README.md", "r", encoding="utf-8") as fid:
-  long_description = fid.read()
+__version__ = '2.0.0'
 
 ext_modules = [
     Pybind11Extension(
@@ -17,28 +12,6 @@ ext_modules = [
 ]
 
 setup(
-    name='mesh2sdf',
-    version=__version__,
-    author='Peng-Shuai Wang',
-    author_email='wangps@hotmail.com',
-    url='https://github.com/wang-ps/mesh2sdf',
-    description='Compute the signed distance field from an input mesh',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=['mesh2sdf'],
-    package_dir={'mesh2sdf': 'mesh2sdf'},
     ext_modules=ext_modules,
     cmdclass={'build_ext': build_ext},
-    zip_safe=False,
-    python_requires='>=3.6',
-    install_requires=[
-        "numpy",
-        "trimesh",
-        'scikit-image',
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
 )
